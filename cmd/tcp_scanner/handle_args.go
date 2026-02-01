26a2b92659c570ae6b56ef9d3caba3eeadbcfb9b
CodeZeroSugar <icvelder@gmail.com>
2026-01-31 22:40:40 -0600

    implemented basic command line options

--- cmd/tcp_scanner/handle_args.go
+++ cmd/tcp_scanner/handle_args.go
@@ -1,23 +1,50 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"strings"
+)
+
+type portMode int
+
+const (
+	single portMode = iota
+	selection
+	series
+)
 
 type params struct {
-	target string
-	ports  int
-	root   bool
+	target   string
+	ports    []string
+	portMode portMode
 }
 
-func handleFlags() (params, error) {
+func handleFlags() params {
 	var params params
+	var targetVar string
+	var portsVar string
+	flag.StringVar(&targetVar, "t", "127.0.0.1", "The IP Address you want to scan. Defaults to loopback.")
+	flag.StringVar(&portsVar, "p", "1-1023", "Input a single port to scan only that port.\nSeparate ports with commas to scan those specific ports.\nProvide a range like '1-500' to scan all ports in that range.\nDefault is common ports.")
 
-	targetPtr := flag.String("t", "127.0.0.1", "The IP Address you want to scan. Defaults to loopback.")
+	flag.Parse()
+	params.target = targetVar
 
-	func init {
-		const (
-			defaultPorts =	 
-		)
-	portsPtr := flag.Int("p", 1023, "")
+	if strings.Contains(portsVar, ",") {
+		params.portMode = selection
+		splitPorts := strings.Split(portsVar, ",")
 
+		trimmedPorts := make([]string, 0)
+		for _, port := range splitPorts {
+			trimmedPorts = append(trimmedPorts, strings.TrimSpace(port))
+		}
+		params.ports = trimmedPorts
+	} else if strings.Contains(portsVar, "-") {
+		params.portMode = series
+		params.ports = append(params.ports, strings.TrimSpace(portsVar))
+	} else {
+		params.portMode = single
+		params.ports = append(params.ports, portsVar)
 	}
+
+	return params
 }
--- cmd/tcp_scanner/main.go
+++ cmd/tcp_scanner/main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -14,35 +15,81 @@ const (
 )
 
 func main() {
-	args := os.Args[1:]
-	target := args[0]
+	params := handleFlags()
 
-	fmt.Printf("Scanning '%s' ports 1-%d\n", target, Ports)
-
-	ip := net.ParseIP(target)
+	ip := net.ParseIP(params.target)
 	if ip == nil {
-		log.Fatalf("failed to parse IP from: %s", target)
+		log.Fatalf("failed to parse IP from: %s", params.target)
+	}
+
+	var portLen int
+	var p []int
+
+	switch params.portMode {
+	case single:
+		portLen = 1
+		numStr := params.ports[0]
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			log.Fatalf("invalid integer assigned to port: %s", err)
+		}
+		p = append(p, num)
+	case selection:
+		portLen = len(params.ports)
+		for _, n := range params.ports {
+			num, err := strconv.Atoi(n)
+			if err != nil {
+				log.Fatalf("invalid integer assigned to port: %s", err)
+			}
+			p = append(p, num)
+		}
+	case series:
+		splitPorts := strings.Split(params.ports[0], "-")
+		numLow, err := strconv.Atoi(splitPorts[0])
+		if err != nil {
+			log.Fatalf("invalid integer assigned to port: %s", err)
+		}
+		numHigh, err := strconv.Atoi(splitPorts[1])
+		if err != nil {
+			log.Fatalf("invalid integer assigned to port: %s", err)
+		}
+		portLen = numHigh - 1
+		p = append(p, numLow, numHigh)
+
+	default:
+		log.Fatalf("Invalid format for ports")
 	}
 
 	taskQueue := make(chan PortScanTask)
-	taskResults := make(chan PortScanResults, Ports)
+	taskResults := make(chan PortScanResults, portLen)
 
 	for range Workers {
 		go scan(taskQueue, taskResults)
 	}
-	for i := range Ports {
-		if i == 0 {
-			continue
+
+	fmt.Printf("Scanning %d ports...\n", portLen)
+
+	if params.portMode == series {
+		for i := p[0]; i < p[1]; i++ {
+			task := PortScanTask{
+				TargetIP: ip.To4(),
+				Port:     i,
+			}
+			taskQueue <- task
 		}
-		task := PortScanTask{
-			TargetIP: ip.To4(),
-			Port:     i,
+	} else {
+		for _, i := range p {
+			task := PortScanTask{
+				TargetIP: ip.To4(),
+				Port:     i,
+			}
+			taskQueue <- task
 		}
-		taskQueue <- task
 	}
+
 	close(taskQueue)
 	fmt.Printf("Scan Results for: %v\n", ip)
-	for range Ports - 1 {
+	for range portLen {
 		result := <-taskResults
 		if result.State == PortStateOpen {
 			fmt.Printf("Port: %-5d | State: %v | Error: %v\n", result.Port, result.State.String(), result.ErrorInfo)
